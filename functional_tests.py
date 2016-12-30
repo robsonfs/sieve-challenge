e6f6875e27d90a688fb83d7572b6595f415f2ea4
Robson Fernandes <robfds@gmail.com>
2016-12-30 14:11:02 -0300

    describing use cases for functional tests

---
+++ functional_tests.py
@@ -0,0 +1,20 @@
+# Uma chamada ao script via terminal sem nenhum parâmetro resulta em um menu
+# no qual o usuário poderá escolher uma das funcionalidades disponíveis.
+
+# Alternativamente, um usuário pode passar um parâmetro e a funcionalida
+# correspondente será executada ou um uma mensagem de erro será exibida caso o
+# parâmetro não seja conhecido.
+
+# Ao escolher a opção que ativa o crawler, o programa pergunta se o arquivo de saída
+# deverá ter um nome padrão ou se o usuário irá informar um nome personalizado.
+
+# Quando o usuário opta por um nome personlizado e o nome escolhido coincida com
+# um nome de arquivo já existente, o programa pergunta ao usuário se ele
+# realmente deseja sobrescrever o arquivo existente ou prefere escolher outro nome.
+
+# Quando o usuário opta pelo nome padrão, um arquivo com o nome
+# products_aaaammddhhmmss.csv é gerado no diretorio outputs
+
+# O arquivo gerado contém dados de todos os produtos do site scaneado
+
+# O arquivo gerado não contém entradas duplicadas
